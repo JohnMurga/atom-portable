fa4f52127b4fa78b3b0ddc82601b2676c2e16893
John de Murga <john@murga.org>
2015-11-10 20:15:06 +0800

    Updated to Atom beta 2

--- 0-download-archives.bat
+++ 0-download-archives.bat
@@ -9,5 +9,5 @@ mkdir downloads 2> nul
 set WGET=cscript /nologo tools\wget.js
 
 %WGET% "http://www.7-zip.org/a/7za920.zip" ".\\downloads\\7za920.zip"
-%WGET% "https://github.com/atom/atom/releases/download/v1.2.0-beta1/atom-windows.zip" ".\\downloads\\atom-windows.zip"
+%WGET% "https://github.com/atom/atom/releases/download/v1.2.0-beta2/atom-windows.zip" ".\\downloads\\atom-windows.zip"
 %WGET% "https://github.com/git-for-windows/git/releases/download/v2.6.2.windows.1/PortableGit-2.6.2-32-bit.7z.exe" "downloads\\PortableGit.7z"
