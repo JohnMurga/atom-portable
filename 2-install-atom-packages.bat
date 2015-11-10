e05c97a5981ed36dc231afc9e241e15ec22190b9
John de Murga <john@murga.org>
2015-11-10 20:16:53 +0800

    Sub optimal but better path length check

--- 2-install-atom-packages.bat
+++ 2-install-atom-packages.bat
@@ -6,6 +6,13 @@ echo Setting Atom up with some cool default packages
 set ATOM_HOME=%~dp0\packages\Atom\Data\AtomProfile
 
 cmd /c scripts\0-install-packages.bat
+
+echo Calculating max path length from current after package install
+dir /b /s .\packages\Atom 2> nul | cscript /nologo .\tools\maxLen.js
+
 cmd /c scripts\1-fix-known-issues.bat
 cmd /c scripts\2-optimize-packages.bat
 cmd /c scripts\3-flatten-packages.bat
+
+echo Calculating max path length from current after package flatten
+dir /b /s .\packages\Atom | cscript /nologo .\tools\maxLen.js
