254e90ba11c640d5fcce1b61c23c912dbf27f92a
John de Murga <john@murga.org>
2015-11-12 00:30:26 +0800

    Slightly better path length measurements ... Not perfect yet

--- 2-install-atom-packages.bat
+++ 2-install-atom-packages.bat
@@ -10,14 +10,13 @@ cmd /c scripts\0-install-packages.bat
 echo Calculating max path length from current after package install
 call scripts\free-drive FREE_DRIVE
 subst %FREE_DRIVE%: .\packages\Atom
-
-dir /b /s %FREE_DRIVE%:\ 2>&1 | cscript /nologo .\tools\maxLen.js
+robocopy /e %FREE_DRIVE%: ..\dummy /MIR /FP /NC /NS /NDL /NJH /NJS /L | cscript /nologo .\tools\maxLen.js
 
 cmd /c scripts\1-fix-known-issues.bat
 cmd /c scripts\2-optimize-packages.bat
 cmd /c scripts\3-flatten-packages.bat
 
 echo Calculating max path length from current after package flatten
-dir /b /s %FREE_DRIVE%:\ 2> nul | cscript /nologo .\tools\maxLen.js
+robocopy /e %FREE_DRIVE%: ..\dummy /MIR /FP /NC /NS /NDL /NJH /NJS /L | cscript /nologo .\tools\maxLen.js
 
 subst %FREE_DRIVE%: /D > nul
