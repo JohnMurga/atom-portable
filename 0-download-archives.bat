f5135e2344d827dfe0d29db2214e2ba12fcf03c5
John de Murga <john@murga.org>
2015-11-12 13:03:17 +0800

    Upgraded to Windows Git v2.6.3 and Atom v1.2.0-beta3

--- 0-download-archives.bat
+++ 0-download-archives.bat
@@ -1,7 +1,9 @@
 @echo off
 REM Copyright 2014-2015 John de Murga - Licensed under the GPLv2
 
+echo -----------------------------
 echo Downloading required archives
+echo -----------------------------
 
 rmdir /s /q downloads 2> nul
 mkdir downloads 2> nul
@@ -9,5 +11,5 @@ mkdir downloads 2> nul
 set WGET=cscript /nologo tools\wget.js
 
 %WGET% "http://www.7-zip.org/a/7za920.zip" ".\\downloads\\7za920.zip"
-%WGET% "https://github.com/atom/atom/releases/download/v1.2.0-beta2/atom-windows.zip" ".\\downloads\\atom-windows.zip"
-%WGET% "https://github.com/git-for-windows/git/releases/download/v2.6.2.windows.1/PortableGit-2.6.2-32-bit.7z.exe" "downloads\\PortableGit.7z"
+%WGET% "https://github.com/atom/atom/releases/download/v1.2.0-beta3/atom-windows.zip" ".\\downloads\\atom-windows.zip"
+%WGET% "https://github.com/git-for-windows/git/releases/download/v2.6.3.windows.1/PortableGit-2.6.3-32-bit.7z.exe" "downloads\\PortableGit.7z"
