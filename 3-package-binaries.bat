15faafefd79d46d1af49fae1709e4ec404a7f54a
John de Murga <john@murga.org>
2015-11-20 12:07:10 +0800

    Clean the Atom profile better and include compile cache for performance

--- 3-package-binaries.bat
+++ 3-package-binaries.bat
@@ -7,10 +7,16 @@ echo --------------------------------------------------
 
 rmdir /s /q .\packages\Atom\Data\AtomProfile\.apm 2> nul
 rmdir /s /q .\packages\Atom\Data\AtomProfile\.node-gyp 2> nul
-rmdir /s /q .\packages\Atom\Data\AtomProfile\compile-cache 2> nul
+rmdir /s /q .\packages\Atom\Data\AtomProfile\local-history 2> nul
+rmdir /s /q .\packages\Atom\Data\AtomProfile\local-storage 2> nul
+rmdir /s /q .\packages\Atom\Data\AtomProfile\storage 2> nul
+del   /f /q .\packages\Atom\Data\AtomProfile\config.cson.bak 2> nul
+
+:: Keep the compile cache if present
+REM rmdir /s /q .\packages\Atom\Data\AtomProfile\compile-cache 2> nul
 
 echo Creating .\packages\AtomPortable.7z ...
 %~dp0\tools\7Zip\7za.exe a -t7z -m0=lzma -mx=9 -mfb=256 -md=64m -ms=on -r .\packages\AtomPortable-Windows.7z .\packages\Atom\* > nul
 
-:: echo Creating .\packages\AtomPortable.zip ...
+:: echo Creating smallest possible .\packages\AtomPortable.zip ...
 :: %~dp0\tools\7Zip\7za.exe a -mm=Deflate -mfb=256 -mpass=15 -r .\packages\AtomPortable-Windows.zip .\packages\Atom\* > nul
