da6835255349b10b70588f1ee146494f8678df30
John de Murga <john@murga.org>
2015-11-20 23:14:18 +0800

    Updated to latest Atom and removed linter-eslint fix (as my patch was accepted)

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -2,15 +2,12 @@ echo "Cleaning existing profile"
 rm -rf ~/.atom
 mkdir ~/.atom
 cd ~/.atom
-echo "Downloading and installing Atom 1.2"
-wget https://github.com/atom/atom/releases/download/v1.2.0/atom-amd64.deb
+echo "Downloading and installing Atom 1.2.3"
+wget https://github.com/atom/atom/releases/download/v1.2.3/atom-amd64.deb
 sudo dpkg -i ./atom-amd64.deb
 echo "Downloading and installing companion packages"
 wget https://raw.githubusercontent.com/JohnMurga/atom-portable/master/scripts/packages.txt
 apm install `cat packages.txt`
-echo "Downloading a version of linter-eslint which works"
-apm install linter-eslint@3.1.1
-sed -i -- 's/"version"\: "3.1.1"/"version"\: "5.1.0"/g' packages/linter-eslint/package.json
 echo "Downloading AtomPortable config"
 wget https://raw.githubusercontent.com/JohnMurga/atom-portable/master/template/Data/AtomProfile/config.cson
 wget https://raw.githubusercontent.com/JohnMurga/atom-portable/master/template/Data/AtomProfile/init.coffee
