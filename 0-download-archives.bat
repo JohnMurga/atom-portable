eb2932f3590ea98068f0cdf5f929b813b8086744
John de Murga <john@murga.org>
2016-01-15 01:04:55 +0800

    Updated versions

--- 0-download-archives.bat
+++ 0-download-archives.bat
@@ -11,6 +11,6 @@ mkdir downloads 2> nul
 set WGET=cscript /nologo tools\wget.js
 
 %WGET% "http://www.7-zip.org/a/7za920.zip" ".\\downloads\\7za920.zip"
-%WGET% "https://github.com/Maximus5/ConEmu/releases/download/v15.12.24/ConEmuPack.151224.7z" ".\\downloads\\ConEmu.7z"
-%WGET% "https://github.com/atom/atom/releases/download/v1.4.0-beta3/atom-windows.zip" ".\\downloads\\atom-windows.zip"
+%WGET% "https://github.com/Maximus5/ConEmu/releases/download/v16.01.11/ConEmuPack.160111.7z" ".\\downloads\\ConEmu.7z"
+%WGET% "https://github.com/atom/atom/releases/download/v1.5.0-beta0/atom-windows.zip" ".\\downloads\\atom-windows.zip"
 %WGET% "https://github.com/git-for-windows/git/releases/download/v2.7.0.windows.1/PortableGit-2.7.0-32-bit.7z.exe" "downloads\\PortableGit.7z"
