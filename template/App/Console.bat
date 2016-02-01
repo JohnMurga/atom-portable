2dc09e03217921c4328f1d3a484697ed07a05587
John de Murga <john@murga.org>
2016-02-01 03:40:48 -0800

    Solved two issues affecting Win10 users (use of "start" and "where")

--- template/App/Console.bat
+++ template/App/Console.bat
@@ -1,20 +1,30 @@
 @echo off
 :: Copyright 2014-2015 John de Murga - Licensed under the GPLv2
 
-FOR /F "tokens=* USEBACKQ" %%F IN (`where bash`) DO (
-	SET BASH_PATH=%%F
+set START_DIR=%CD%
+
+cd /d "%~dp0\..\..\git\usr\bin"
+
+FOR /F "tokens=* USEBACKQ" %%F IN (`.\which.exe bash`) DO (
+    SET BASH_PATH=%%F
 )
+
+FOR /F "tokens=* USEBACKQ" %%F IN (`.\cygpath.exe -w "%BASH_PATH%"`) DO (
+    SET BASH_PATH=%%F
+)
+
+cd /d "%START_DIR%"
 
 cls
 
 if "%BASH_PATH%" EQU "" (
     if "%ConEmuANSI%" EQU "ON" (
-        call start "" /B /W /D "%CD%" cmd /k "%~dp0\ConEmu\ConEmu\CmdInit.cmd"
+        cmd /k "%~dp0\ConEmu\ConEmu\CmdInit.cmd"
     ) else (
         set PROMPT=%username%@%computername% $p$_^> 
-        call start "" /B /W /D "%CD%" cmd
+        cmd
     )
 ) else (
-	set PROMPT=%username%@%computername% $p$_^> 
-    call start "" /B /W /D "%CD%" "%BASH_PATH%" --login -i -s
+    set PROMPT=%username%@%computername% $p$_^> 
+    "%BASH_PATH%" --login -i -s
 )
