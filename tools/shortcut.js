ddd62f150fc710e087b9558896b8441eeaff52f1
John de Murga <john@murga.org>
2016-04-05 08:51:21 +0800

    New shortcut tooling

---
+++ tools/shortcut.js
@@ -0,0 +1,23 @@
+// jscs:disable requireMultipleVarDecl
+// jscs:disable requireVarDeclFirst
+/* eslint new-cap:0 */
+/* global ActiveXObject WScript */
+
+/* Copyright 2014-2016 John de Murga - Licensed under the GPLv2 */
+
+function shortcut( source, destination, description ) {
+    "use strict";
+
+    var oWS = new ActiveXObject("WScript.Shell");
+
+    var oLink = oWS.CreateShortcut( destination );
+    oLink.TargetPath = source;
+    // oLink.Arguments = "";
+    oLink.Description = description;
+    oLink.IconLocation = source + ", 0";
+    oLink.WorkingDirectory = ".";
+    oLink.Save();
+}
+
+WScript.StdOut.WriteLine("Creating shortcut for : " + WScript.Arguments( 0 ) );
+shortcut( WScript.Arguments( 0 ), WScript.Arguments( 1 ), WScript.Arguments( 2 ) );
