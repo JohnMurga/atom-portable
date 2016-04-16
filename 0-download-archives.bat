d61c4c99dc1a04edde8b3c7ecdd65486ecb868ad
John de Murga <john@murga.org>
2016-04-16 15:13:34 +0800

    Updated to latest Node LTS (4.4.3)

--- 0-download-archives.bat
+++ 0-download-archives.bat
@@ -11,7 +11,7 @@ mkdir downloads 2> nul
 set WGET=cscript /nologo tools\wget.js
 
 %WGET% "http://www.7-zip.org/a/7z1514.msi" ".\\downloads\\7Zip.msi"
-%WGET% "https://nodejs.org/dist/v4.4.2/node-v4.4.2-x86.msi" ".\\downloads\\node.msi"
+%WGET% "https://nodejs.org/dist/v4.4.3/node-v4.4.3-x86.msi" ".\\downloads\\node.msi"
 %WGET% "http://gnome.mirrors.pair.com/pub/gnome/binaries/win32/meld/3.14/Meld-3.14.2-win32.msi" ".\\downloads\\meld.msi"
 %WGET% "https://github.com/Maximus5/ConEmu/releases/download/v16.04.13/ConEmuPack.160413.7z" ".\\downloads\\ConEmu.7z"
 %WGET% "https://github.com/atom/atom/releases/download/v1.7.1/atom-windows.zip" ".\\downloads\\atom-windows.zip"
