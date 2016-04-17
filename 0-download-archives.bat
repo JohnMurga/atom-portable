c1230ca2ed3a93d320360d5256093854fdf21e25
John de Murga <john@murga.org>
2016-04-18 00:49:04 +0800

    Updated ConEmu

--- 0-download-archives.bat
+++ 0-download-archives.bat
@@ -1,5 +1,5 @@
 @echo off
-REM Copyright 2014-2015 John de Murga - Licensed under the GPLv2
+REM Copyright 2014-2016 John de Murga - Licensed under the GPLv2
 
 echo -----------------------------
 echo Downloading required archives
@@ -13,6 +13,6 @@ set WGET=cscript /nologo tools\wget.js
 %WGET% "http://www.7-zip.org/a/7z1514.msi" ".\\downloads\\7Zip.msi"
 %WGET% "https://nodejs.org/dist/v4.4.3/node-v4.4.3-x86.msi" ".\\downloads\\node.msi"
 %WGET% "http://gnome.mirrors.pair.com/pub/gnome/binaries/win32/meld/3.14/Meld-3.14.2-win32.msi" ".\\downloads\\meld.msi"
-%WGET% "https://github.com/Maximus5/ConEmu/releases/download/v16.04.13/ConEmuPack.160413.7z" ".\\downloads\\ConEmu.7z"
+%WGET% "https://github.com/Maximus5/ConEmu/releases/download/v16.04.16/ConEmuPack.160416.7z" ".\\downloads\\ConEmu.7z"
 %WGET% "https://github.com/atom/atom/releases/download/v1.7.1/atom-windows.zip" ".\\downloads\\atom-windows.zip"
 %WGET% "https://github.com/git-for-windows/git/releases/download/v2.8.1.windows.1/PortableGit-2.8.1-32-bit.7z.exe" "downloads\\PortableGit.7z"
