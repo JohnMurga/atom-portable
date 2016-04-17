aa928dc94ca52d1e32cadbe25c5fc70fe3b7ef2d
John de Murga <john@murga.org>
2016-04-17 12:55:58 +0800

    Core archives packaged correctly now

--- 3-package-binaries.bat
+++ 3-package-binaries.bat
@@ -10,7 +10,6 @@ rmdir /s /q .\packages\Atom\Data\AtomProfile\.node-gyp 2> nul
 rmdir /s /q .\packages\Atom\Data\AtomProfile\local-history 2> nul
 rmdir /s /q .\packages\Atom\Data\AtomProfile\local-storage 2> nul
 rmdir /s /q .\packages\Atom\Data\AtomProfile\storage 2> nul
-rmdir /s /q .\packages\LLVM 2> nul
 del   /f /q .\packages\Atom\Data\AtomProfile\config.cson.bak 2> nul
 
 echo Performing BASH fixups
@@ -19,12 +18,21 @@ echo Performing BASH fixups
 echo Copying launchers
 copy .\tools\Launchers\*.exe .\packages
 
-echo Creating .\packages\AtomPortable-core.7z ... PLEASE WAIT
-%~dp0\tools\7Zip\7z.exe a -t7z -mx -m0=lzma -mlc=8 -myx=9 -mmc=1000000 -mfb=273 -md=128m -ms=on -r .\packages\AtomPortable-Windows.7z .\packages\Atom\*
+set PACK=%~dp0\tools\7Zip\7z.exe a -t7z -mx -m0=lzma -mlc=8 -myx=9 -mmc=1000000 -mfb=273 -md=128m-ms=on
+
+echo Creating .\packages\AtomPortable-Part1-Core.7z ... PLEASE WAIT
+%PACK% .\packages\AtomPortable-Part1-Core.7z -r .\packages\Atom
+
+echo Creating .\packages\AtomPortable-Part2-Addons.7z ... PLEASE WAIT
+%PACK% .\packages\AtomPortable-Part2-Addons.7z -r .\packages\git .\packages\meld .\packages\nodejs -x!Atom\App\meld
+%PACK% .\packages\AtomPortable-Part2-Addons.7z .\packages\Atom.exe -x!Atom\App\Atom\atom.exe
+%PACK% .\packages\AtomPortable-Part2-Addons.7z .\packages\ConEmu-BASH.exe
+%PACK% .\packages\AtomPortable-Part2-Addons.7z .\packages\ConEmu-CMD.exe
+%PACK% .\packages\AtomPortable-Part2-Addons.7z .\packages\Meld.exe
 
 :: ** For use on 8GB+ machines with and recent versions of 64Bit 7Zip **
-:: %~dp0\tools\7Zip\7z.exe a -t7z -mx -m0=lzma -mlc=8 -myx=9 -mmc=1000000 -mfb=273 -md=580m -ms=on -r .\packages\AtomPortable-Windows.7z .\packages\Atom\*
+:: %~dp0\tools\7Zip\7z.exe a -t7z -mx -m0=lzma -mlc=8 -myx=9 -mmc=1000000 -mfb=273 -md=580m -ms=on -r
 
 :: ** For creating the tightest possible ZIP files **
 :: echo Creating smallest possible .\packages\AtomPortable.zip ...
-:: %~dp0\tools\7Zip\7za.exe a -mm=Deflate -mfb=256 -mpass=15 -r .\packages\AtomPortable-Windows.zip .\packages\Atom\* > nul
+:: %~dp0\tools\7Zip\7za.exe a -mm=Deflate -mfb=256 -mpass=15 -r
