a85c07616c362e8f662cf47b541dd103b2b6409b
John de Murga <john@murga.org>
2016-05-04 02:28:11 +0800

    Tidied up Ubuntu/Debian script after testing

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -1,28 +1,35 @@
+echo ""
 echo "Cleaning existing profile"
+echo ""
 
 rm -rf ~/.atom
 mkdir ~/.atom
 cd ~/.atom
 
+echo ""
 echo "Downloading and installing Atom"
+echo ""
 
 wget https://github.com/atom/atom/releases/download/v1.7.3/atom-amd64.deb
 sudo dpkg -i ./atom-amd64.deb
+sudo apt-get -f install
 
+echo ""
 echo "Downloading and installing companion packages"
+echo ""
 
 wget https://raw.githubusercontent.com/JohnMurga/atom-portable/master/scripts/packages.txt
 
-for packageName in $(cat scripts/packages.txt)
+for packageName in $(cat packages.txt)
 do
     echo -n "$packageName "
-    apm install $packageName
+    apm install --color false $packageName > /dev/null
     if [ $? -ne 0 ]
     then
-        apm install $packageName
+        apm install --color false $packageName > /dev/null
         if [ $? -ne 0 ]
         then
-            apm install $packageName
+            apm install --color false $packageName > /dev/null
             if [ $? -ne 0 ]
             then
                 echo "FAILED to install $packageName"
@@ -31,7 +38,9 @@ do
     fi
 done
 
+echo ""
 echo "Downloading AtomPortable config"
+echo ""
 
 wget https://raw.githubusercontent.com/JohnMurga/atom-portable/master/template/Data/AtomProfile/config.cson
 wget https://raw.githubusercontent.com/JohnMurga/atom-portable/master/template/Data/AtomProfile/init.coffee
@@ -39,9 +48,13 @@ wget https://raw.githubusercontent.com/JohnMurga/atom-portable/master/template/D
 wget https://raw.githubusercontent.com/JohnMurga/atom-portable/master/template/Data/AtomProfile/styles.less
 wget https://raw.githubusercontent.com/JohnMurga/atom-portable/master/template/Data/AtomProfile/.eslintrc
 
+echo ""
 echo "Tweaking settings for Ubuntu"
+echo ""
 
 sed -i -- 's/Console\;exit//g' config.cson
 sed -i -- 's/ConEmu.bat/gnome-terminal/g' config.cson
 
+echo ""
 echo "All done !!"
+echo ""
