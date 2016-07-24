ce9d686b7b388f243a56922a8d6bd43f54ba4894
John de Murga <john@murga.org>
2016-07-24 21:26:31 +0800

    Version updates for many programs

--- 0-download-archives.bat
+++ 0-download-archives.bat
@@ -11,10 +11,11 @@ mkdir downloads 2> nul
 set WGET=cscript /nologo tools\wget.js
 
 %WGET% "http://www.7-zip.org/a/7z1602.msi" ".\\downloads\\7Zip.msi"
-%WGET% "https://nodejs.org/dist/v4.4.5/node-v4.4.5-x86.msi" ".\\downloads\\node.msi"
-%WGET% "http://repo.msys2.org/msys/i686/sqlite-3.8.8.2-1-i686.pkg.tar.xz" ".\\downloads\\sqlite.tar.xz"
-%WGET% "http://repo.msys2.org/msys/i686/libsqlite-3.8.8.2-1-i686.pkg.tar.xz" ".\\downloads\\sqlite-lib.tar.xz"
-%WGET% "https://download.gnome.org/binaries/win32/meld/3.16/Meld-3.16.0-win32.msi" ".\\downloads\\meld.msi"
-%WGET% "https://github.com/Maximus5/ConEmu/releases/download/v16.06.19/ConEmuPack.160619.7z" ".\\downloads\\ConEmu.7z"
-%WGET% "https://github.com/atom/atom/releases/download/v1.9.0-beta0/atom-windows.zip" ".\\downloads\\atom-windows.zip"
-%WGET% "https://github.com/git-for-windows/git/releases/download/v2.9.0.windows.1/PortableGit-2.9.0-32-bit.7z.exe" "downloads\\PortableGit.7z"
+%WGET% "https://nodejs.org/dist/v4.4.7/node-v4.4.7-x86.msi" ".\\downloads\\Node.msi"
+%WGET% "http://repo.msys2.org/msys/i686/sqlite-3.8.8.2-1-i686.pkg.tar.xz" ".\\downloads\\Sqlite.tar.xz"
+%WGET% "http://repo.msys2.org/msys/i686/libsqlite-3.8.8.2-1-i686.pkg.tar.xz" ".\\downloads\\Sqlite-lib.tar.xz"
+%WGET% "http://ftp.nara.wide.ad.jp/pub/X11/GNOME/binaries/win32/meld/3.16/Meld-3.16.0-win32.msi" ".\\downloads\\Meld.msi"
+%WGET% "https://www.fossil-scm.org/download/fossil-w32-1.35.zip" ".\\downloads\\Fossil.zip"
+%WGET% "https://github.com/Maximus5/ConEmu/releases/download/v16.07.10/ConEmuPack.160710.7z" ".\\downloads\\ConEmu.7z"
+%WGET% "https://github.com/atom/atom/releases/download/v1.9.0-beta0/atom-windows.zip" ".\\downloads\\Atom-windows.zip"
+%WGET% "https://github.com/git-for-windows/git/releases/download/v2.9.2.windows.1/PortableGit-2.9.2-32-bit.7z.exe" "downloads\\PortableGit.7z"
