9b75bed7c54b096218f52aa4f3f9e3ed9fceac07
John de Murga <john@murga.org>
2016-10-09 17:24:21 +0800

    Updated 7Zip/Node/ConEmu/Git

--- 0-download-archives.bat
+++ 0-download-archives.bat
@@ -10,13 +10,13 @@ mkdir downloads 2> nul
 
 set WGET=cscript /nologo tools\wget.js
 
-%WGET% "http://www.7-zip.org/a/7z1602.msi" ".\\downloads\\7Zip.msi"
-%WGET% "https://nodejs.org/dist/v4.5.0/node-v4.5.0-x86.msi" ".\\downloads\\Node.msi"
+%WGET% "http://www.7-zip.org/a/7z1604.msi" ".\\downloads\\7Zip.msi"
+%WGET% "https://nodejs.org/dist/v4.6.0/node-v4.6.0-x86.msi" ".\\downloads\\Node.msi"
 %WGET% "http://repo.msys2.org/msys/i686/sqlite-3.8.8.2-1-i686.pkg.tar.xz" ".\\downloads\\Sqlite.tar.xz"
 %WGET% "http://repo.msys2.org/msys/i686/libsqlite-3.8.8.2-1-i686.pkg.tar.xz" ".\\downloads\\Sqlite-lib.tar.xz"
 %WGET% "http://ftp.nara.wide.ad.jp/pub/X11/GNOME/binaries/win32/meld/3.16/Meld-3.16.2-win32.msi" ".\\downloads\\Meld.msi"
 %WGET% "https://www.fossil-scm.org/download/fossil-w32-1.35.zip" ".\\downloads\\Fossil.zip"
-%WGET% "https://github.com/Maximus5/ConEmu/releases/download/v16.09.13/ConEmuPack.160913.7z" ".\\downloads\\ConEmu.7z"
+%WGET% "https://github.com/Maximus5/ConEmu/releases/download/v16.10.02/ConEmuPack.161002.7z" ".\\downloads\\ConEmu.7z"
 %WGET% "https://github.com/atom/atom/releases/download/v1.10.2/atom-windows.zip" ".\\downloads\\Atom-windows.zip"
-%WGET% "https://github.com/git-for-windows/git/releases/download/v2.10.0.windows.1/PortableGit-2.10.0-32-bit.7z.exe" "downloads\\PortableGit.7z"
+%WGET% "https://github.com/git-for-windows/git/releases/download/v2.10.1.windows.1/PortableGit-2.10.1-32-bit.7z.exe" "downloads\\PortableGit.7z"
 %WGET% "https://raw.githubusercontent.com/amix/vimrc/master/vimrcs/basic.vim" ".\\downloads\\.vimrc"
