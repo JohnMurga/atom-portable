35f3654ab934a9e66c01da5a7a8220cf7050d7fb
John de Murga <john@murga.org>
2017-05-08 08:49:44 +0800

    Updaated to lastest stable and working GCC (7.1.0)

--- 4-build-gcc-pack.bat
+++ 4-build-gcc-pack.bat
@@ -8,13 +8,15 @@ echo ------------------------
 set WGET=cscript /nologo tools\wget.js
 set CURL=.\packages\Git\usr\bin\curl.exe -L
 
+::    %CURL% "http://download.sourceforge.net/project/mingw-w64/Toolchains targetting Win32/Personal Builds/mingw-builds/4.8.5/threads-posix/sjlj/i686-4.8.5-release-posix-sjlj-rt_v4-rev0.7z" -o ".\\downloads\\gcc.7z"
 ::    %CURL% "http://download.sourceforge.net/project/mingw-w64/Toolchains targetting Win32/Personal Builds/mingw-builds/4.9.3/threads-posix/sjlj/i686-4.9.3-release-posix-sjlj-rt_v4-rev1.7z" -o ".\\downloads\\gcc.7z"
 ::    %CURL% "http://download.sourceforge.net/project/mingw-w64/Toolchains targetting Win32/Personal Builds/mingw-builds/5.3.0/threads-posix/sjlj/i686-5.3.0-release-posix-sjlj-rt_v4-rev0.7z" -o ".\\downloads\\gcc.7z"
 ::    %CURL% "http://download.sourceforge.net/project/mingw-w64/Toolchains targetting Win32/Personal Builds/mingw-builds/6.2.0/threads-posix/sjlj/i686-6.2.0-release-posix-sjlj-rt_v5-rev1.7z" -o ".\\downloads\\gcc.7z"
 ::    %CURL% "http://download.sourceforge.net/project/mingw-w64/Toolchains targetting Win32/Personal Builds/mingw-builds/6.3.0/threads-posix/sjlj/i686-6.3.0-release-posix-sjlj-rt_v5-rev1.7z" -o ".\\downloads\\gcc.7z"
+::    %CURL% "http://download.sourceforge.net/project/mingw-w64/Toolchains targetting Win32/Personal Builds/mingw-builds/7.1.0/threads-win32/sjlj/i686-7.1.0-release-win32-sjlj-rt_v5-rev0.7z" -o ".\\downloads\\gcc.7z"
 
 IF NOT EXIST ".\\downloads\\gcc.7z" (
-    %CURL% "http://download.sourceforge.net/project/mingw-w64/Toolchains targetting Win32/Personal Builds/mingw-builds/4.8.5/threads-posix/sjlj/i686-4.8.5-release-posix-sjlj-rt_v4-rev0.7z" -o ".\\downloads\\gcc.7z"
+    %CURL% "http://download.sourceforge.net/project/mingw-w64/Toolchains targetting Win32/Personal Builds/mingw-builds/7.1.0/threads-win32/sjlj/i686-7.1.0-release-win32-sjlj-rt_v5-rev0.7z" -o ".\\downloads\\gcc.7z"
 ) else (
     echo GCC Download already present
 )
