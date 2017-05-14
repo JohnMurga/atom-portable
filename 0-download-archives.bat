696c2d929904ad0d83455312e547c3ebffe3f6e7
John de Murga <john@murga.org>
2017-05-14 13:12:08 +0800

    Updated to the new SHA safe Git v2.13

--- 0-download-archives.bat
+++ 0-download-archives.bat
@@ -19,5 +19,5 @@ set WGET=cscript /nologo tools\wget.js
 %WGET% "https://www.fossil-scm.org/fossil/uv/fossil-w32-2.2.zip" ".\\downloads\\Fossil.zip"
 %WGET% "https://github.com/Maximus5/ConEmu/releases/download/v17.04.02/ConEmuPack.170402.7z" ".\\downloads\\ConEmu.7z"
 %WGET% "https://github.com/atom/atom/releases/download/v1.16.0/atom-windows.zip" ".\\downloads\\Atom-windows.zip"
-%WGET% "https://github.com/git-for-windows/git/releases/download/v2.12.2.windows.1/PortableGit-2.12.2-32-bit.7z.exe" "downloads\\PortableGit.7z"
+%WGET% "https://github.com/git-for-windows/git/releases/download/v2.13.0.windows.1/PortableGit-2.13.0-32-bit.7z.exe" "downloads\\PortableGit.7z"
 %WGET% "https://raw.githubusercontent.com/amix/vimrc/master/vimrcs/basic.vim" ".\\downloads\\.vimrc"
