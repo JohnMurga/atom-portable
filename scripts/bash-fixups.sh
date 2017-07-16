8b6b650e3077b14a3735bc00c17d002cf103c751
John de Murga <john@murga.org>
2017-07-17 00:19:26 +0800

    Fixed MinTTY config tweak issue

--- scripts/bash-fixups.sh
+++ scripts/bash-fixups.sh
@@ -8,14 +8,14 @@ echo "" >> ./packages/_home/.vimrc
 echo "let g:markdown_fenced_languages = ['css', 'javascript', 'js=javascript', 'json=javascript', 'xml']" >> ./packages/_home/.vimrc
 echo ""
 
+echo "Tweaking minttyrc for readability"
+echo "Transparency=low" >> ./packages/git/etc/minttyrc
+echo "CursorType=block" >> ./packages/git/etc/minttyrc
+echo ""
+
 echo "Deleting some of the larger unreferenced image files in packages"
 cd packages/Atom/Data/AtomProfile
 find . -name *.gif -size +10k -print | xargs -I '{}' sh -c 'echo "DEL {}"; rm {};'
 find . -name *.jpg -size +80k -print | xargs -I '{}' sh -c 'echo "DEL {}"; rm {};'
 find . -name screenshot.png -size +80k -print | xargs -I '{}' sh -c 'echo "DEL {}"; rm {};'
-echo ""
-
-echo "Tweaking minttyrc for readability"
-echo "Transparency=low" >> ./packages/git/etc/minttyrc
-echo "CursorType=block" >> ./packages/git/etc/minttyrc
 echo ""
