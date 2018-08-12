7b8168e7a85130f7b519c0980270a1d6a204a9d6
John de Murga <john@murga.org>
2018-08-13 00:38:54 +0700

    Fix to in app terminal start

--- template/App/Bash.bat
+++ template/App/Bash.bat
@@ -1,9 +1,12 @@
 @echo off
 :: Copyright 2014-2016 John de Murga - Licensed under the GPLv2
 
+IF DEFINED MYPATH GOTO RUN_TERM
+
 call %~dp0\setPaths.bat
 cd /D %HOME%
 
+:RUN_TERM
 :: Remove ConEmu biaries for now
 :: cmd /c "" %~dp0\ConEmu.bat
 
