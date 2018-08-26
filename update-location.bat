df50e011fd580bddbd955ae3cbac3a2c846cd559
John de Murga <john@murga.org>
2018-08-26 21:15:34 +0800

    Tweaked for my purposes

--- update-location.bat
+++ update-location.bat
@@ -10,12 +10,16 @@ mkdir %~dp0\empty
 set WIPEDIR=robocopy /MIR /XJD /XJF /R:1 /W:1 %~dp0\empty
 set UN7ZIP=%~dp0\tools\7Zip\7z.exe x -y
 
-SET DIR_LIST=_home\go _home\npm _home\npm-cache atom git meld go rust 
+SET DIR_LIST=_home\npm _home\npm-cache ^
+atom git meld go rust java ruby python
 SET ARC_LIST=^
 AtomPortable-Part1-Core.7z ^
 AtomPortable-Part2-Addons.7z ^
-AtomPortable-Part3-Rust-Nightly.7z ^
-AtomPortable-Part4-Go-1.11.beta1-64bit.7z
+AtomPortable-Part4-Go-1.11-64bit.7z ^
+AtomPortable-Part3-Rust-stable-i686-pc-windows-gnu.7z ^
+AtomPortable-Part4-Java-11.7z ^
+AtomPortable-Part3-Ruby-2.5.1-2.7z ^
+AtomPortable-Part4-Python-2.7.15.7z
 
 move %TARGET%\atom\Data\AtomProfile\local-history %TARGET%\local-history
 
@@ -28,7 +32,9 @@ rmdir /s /q %~dp0\empty 2> nul
 
 FOR %%G IN (%ARC_LIST%) DO (
 	echo Unpacking %%G
-	%UN7ZIP% %~dp0\packages\%%G -o%TARGET% > nul
+	%UN7ZIP% %~dp0\%%G -o%TARGET% > nul
 )
 
 move %TARGET%\local-history %TARGET%\atom\Data\AtomProfile\local-history
+
+pause
