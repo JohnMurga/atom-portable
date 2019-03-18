bb9414987319fc7944f7ef5349576484f83e2b4d
John de Murga <john@murga.org>
2019-03-18 19:55:28 +0800

    Updated core packages and tidied up

--- 0-download-archives.bat
+++ 0-download-archives.bat
@@ -10,7 +10,7 @@ mkdir downloads 2> nul
 
 set WGET=cscript /nologo tools\wget.js
 
-%WGET% "https://www.7-zip.org/a/7z1806.msi" ".\\downloads\\7Zip.msi"
+%WGET% "https://www.7-zip.org/a/7z1900.msi" ".\\downloads\\7Zip.msi"
 %WGET% "http://repo.msys2.org/msys/i686/rsync-3.1.3-1-i686.pkg.tar.xz" ".\\downloads\\RSync.tar.xz"
 %WGET% "http://repo.msys2.org/msys/i686/sqlite-3.21.0-4-i686.pkg.tar.xz" ".\\downloads\\Sqlite.tar.xz"
 %WGET% "http://repo.msys2.org/msys/i686/libsqlite-3.21.0-4-i686.pkg.tar.xz" ".\\downloads\\libsqlite.tar.xz"
@@ -19,8 +19,8 @@ set WGET=cscript /nologo tools\wget.js
 %WGET% "http://repo.msys2.org/msys/i686/zsh-5.5.1-1-i686.pkg.tar.xz" ".\\downloads\\zsh.tar.xz"
 %WGET% "http://repo.msys2.org/msys/i686/xz-5.2.4-1-i686.pkg.tar.xz" ".\\downloads\\xz.tar.xz"
 %WGET% "https://mirrors.ustc.edu.cn/gnome/binaries/win32/meld/3.20/Meld-3.20.0-mingw.msi" ".\\downloads\\Meld.msi"
-%WGET% "https://fossil-scm.org/index.html/uv/fossil-w32-2.7.zip" ".\\downloads\\Fossil.zip"
-%WGET% "https://github.com/Maximus5/ConEmu/releases/download/v19.01.08/ConEmuPack.190108.7z" ".\\downloads\\ConEmu.7z"
-%WGET% "https://github.com/atom/atom/releases/download/v1.35.0-beta0/atom-windows.zip" ".\\downloads\\Atom-windows.zip"
-%WGET% "https://github.com/git-for-windows/git/releases/download/v2.20.1.windows.1/PortableGit-2.20.1-32-bit.7z.exe" "downloads\\PortableGit.7z"
+%WGET% "https://www.fossil-scm.org/index.html/uv/fossil-w32-2.8.zip" ".\\downloads\\Fossil.zip"
+%WGET% "https://github.com/Maximus5/ConEmu/releases/download/v19.03.10/ConEmuPack.190310.7z" ".\\downloads\\ConEmu.7z"
+%WGET% "https://github.com/atom/atom/releases/download/v1.36.0-beta1/atom-windows.zip" ".\\downloads\\Atom-windows.zip"
+%WGET% "https://github.com/git-for-windows/git/releases/download/v2.21.0.windows.1/PortableGit-2.21.0-32-bit.7z.exe" "downloads\\PortableGit.7z"
 %WGET% "https://raw.githubusercontent.com/amix/vimrc/master/vimrcs/basic.vim" ".\\downloads\\.vimrc"
